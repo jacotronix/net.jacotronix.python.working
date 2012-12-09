c2e18f44905e650d46038ff99013c82b8cfe52eb
Jamie <jamie.d.jackson@gmail.com>
2012-12-09 19:07:02 +0000

    Initial Commit

---
+++ Tweeter/setup.py
@@ -0,0 +1,14 @@
+'''
+Created on 8 Dec 2012
+
+@author: Jamie
+'''
+from distutils.core import setup
+setup(name='tweeter',
+      version='0.0.2', # Next version after the released version
+      description='Twitter Wrapper',
+      author='Jamie Jackson',
+      author_email='jamie@jacobean.net',
+      url='http://blog.jacobean.net',
+      py_modules=['tweeter'],
+      )
