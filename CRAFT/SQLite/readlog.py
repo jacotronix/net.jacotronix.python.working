007a64a6e832292461171908ff90b7d27ed442e4
Jamie <jamie.d.jackson@gmail.com>
2012-12-25 00:06:42 +0000

    Added udpRx.py for listening to arduino udp messages Tinkering with python graphing in realog.py

--- CRAFT/SQLite/readlog.py
+++ CRAFT/SQLite/readlog.py
@@ -7,14 +7,20 @@ import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 from matplotlib import pyplot 
 from matplotlib.dates import date2num
+import matplotlib.dates as mdates
 import sqlite3
 
 datestamps = []
 temps = []
 pressures = []
 
+years    = mdates.YearLocator()   # every year
+months   = mdates.MonthLocator()  # every month
+days    = mdates.DayLocator()
+hours   = mdates.HourLocator()
+daysFmt = mdates.DateFormatter('%a %d %b %y')
 
-conn = sqlite3.connect("test.db", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+conn = sqlite3.connect("/var/local/log/test.db", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 
 sql = 'SELECT datetime as "[timestamp]", temp, pressure FROM logging'
 conn.row_factory = sqlite3.Row
@@ -30,27 +36,34 @@ for row in rows:
     temps.append(row[1])
     pressures.append(row[2])
 
-#------------------------------
-
 dates = date2num(datestamps)
-pyplot.plot_date(dates, temps, 'b-', xdate=True)
-pyplot.plot_date(dates, pressures, 'g-', xdate=True)
-pyplot.title('Temperatures')
-pyplot.xlabel('Date/Time')
-pyplot.ylabel('Temp Deg C')
+fig = pyplot.figure()
+plt0 = fig.add_subplot(111)
+plt1 = fig.add_subplot(211)
+
+plt0.spines['top'].set_color('none')
+plt0.spines['bottom'].set_color('none')
+plt0.spines['left'].set_color('none')
+plt0.spines['right'].set_color('none')
+plt0.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+
+plt1.plot_date(dates, temps, 'b-', xdate=True)
+plt1.set_title("Temperature")
+#ax.xlabel('Date/Time')
+plt1.xaxis.set_major_locator(days)
+plt1.xaxis.set_major_formatter(daysFmt)
+plt1.set_ylabel('Deg C')
+
+plt2 = fig.add_subplot(212)
+plt2.plot_date(dates, pressures, 'g-', xdate=True)
+plt2.set_title("Barometric Pressure")
+plt2.set_ylabel('hPa')
+
+plt2.xaxis.set_major_locator(days)
+plt2.xaxis.set_major_formatter(daysFmt)
+#plt2.xaxis.set_minor_locator(hours)
+plt2.set_xlabel('Date')
+
+fig.autofmt_xdate()
 pyplot.savefig('tempplot.png')
 pyplot.show()
-
-#http://stackoverflow.com/questions/7340547/multiple-data-set-plotting-with-matplotlib-pyplot-plot-date
-#
-#http://matplotlib.org/examples/api/two_scales.html
-#
-#http://klassenresearch.orbs.com/Plotting+with+Python
-#
-#http://matplotlib.org/users/pyplot_tutorial.html
-#
-#http://www.endlesslycurious.com/2011/05/04/basic-graphing-with-matplotlib/
-#
-#http://matplotlib.org/api/pyplot_api.html?highlight=plot_date#matplotlib.pyplot.plot_date
- 
-
